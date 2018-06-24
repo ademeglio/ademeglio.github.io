09fa1677126e8c3ef6d604f65c904fc6a795f595
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-06-24 11:37:03 -0400

    mobile nav improvement

--- scrip.js
+++ scrip.js
@@ -1,4 +1,15 @@
 // nav menu transition
-function myFunction(x) {
-    x.classList.toggle("change");
+function toggleMenu(menuContainer) {
+    
+    const growUp = document.querySelector('#main-nav');
+    const growUpChild = growUp.querySelector('div');
+    
+    if (menuContainer.classList.contains('show')) {
+        growUp.style.height = '0';
+        menuContainer.classList.remove('show');
+    }
+    else {
+        growUp.style.height = growUpChild.offsetHeight + 'px';
+        menuContainer.classList.add('show');
+    }
 }
